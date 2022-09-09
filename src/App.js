6986a447474a78592aaca0f08298c885c878d481
Ken9709 <Kwood9709@gmail.com>
2022-09-08 20:52:17 -0700

    added match history search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { set } from "lodash";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [SearchText, setSearchText] = useState("");
+    const [playerData, setPlayerData] = useState({});
+    const [gameList, setGameList] = useState([]);
+
+    function searchForPlayer(event) {
+        // handle the API call
+
+        axios
+            .get(
+                "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
+                    SearchText +
+                    "?api_key=RGAPI-be5563cb-1fa6-4dda-b870-7974a7cb112b",
+                { mode: "cors" }
+            )
+            .then(function (response) {
+                setPlayerData(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function searchChampMastery(event) {
+        // handle the API call, hit our proxy server
+
+        axios
+            .get("http://localhost:4000/past5Games", {params: {username: SearchText}})
+            //on success, set game list to the response from the server
+            .then(function (response) {
+                setGameList(response.data);
+                //fail, throw error
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    console.log(gameList);
+
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>league of legends search</h1>
+                <input
+                    type="text"
+                    onChange={(e) => setSearchText(e.target.value)}
+                ></input>
+                <button onClick={(e) => searchForPlayer(e)}>
+                    Search for player
+                </button>
+                <button onClick={(e) => searchChampMastery(e)}>
+                    champ data
+                </button>
+                {gameList.length !== 0 ? (
+                    <>
+                        <p> We have data</p>
+                        {gameList.map((gameData, index) => (
+                            <>
+                                <h2>Game {index + 1}</h2>
+                                <div>
+                                    {gameData.info.participants.map(
+                                        (data, participantIndex) => (
+                                            <p>
+                                                player {participantIndex + 1}:{" "}
+                                                {data.summonerName}, KDA:{" "}
+                                                {data.kills} / {data.deaths} /{" "}
+                                                {data.assits}
+                                            </p>
+                                        )
+                                    )}
+                                </div>
+                            </>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        <p>we have no data</p>
+                    </>
+                )}
+            </div>
+            {playerData.name != "{}" ? (
+                <>
+                    <p>Summoner name: {playerData.name}</p>
+                    <p>Summoner level: {playerData.summonerLevel}</p>
+
+                    <p>Champ: </p>
+                </>
+            ) : (
+                <>
+                    <p>no player data</p>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
